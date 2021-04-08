073079dc8dde160e382db54eff9fbffb4e3456c1
Lipr1x <eli2281337@mail.ru>
2021-04-08 22:54:53 +0300

    form corrected

--- TechiqueShopViewProvider/GetListForm.xaml.cs
+++ TechiqueShopViewProvider/GetListForm.xaml.cs
@@ -53,6 +53,26 @@ namespace TechiqueShopViewProvider
             }
         }
 
+        private void ViewButton_Click(object sender, RoutedEventArgs e)
+        {
+            assemblyGrid.Items.Clear();
+            selectComponentGrid.Items.Clear();
+            List<String> selected_components = new List<string>();
+            foreach (var item in CanSelectedComponentListBox.SelectedItems)
+            {
+                selected_components.Add(item.ToString());
+            }
+            var file = list_logic.SaveComponentsToWordFile(new GetListBindingModel { Components = selected_components });
+
+            foreach (var ca in file.ComponentAssembly)
+            {
+                assemblyGrid.Items.Add(ca.AssemblyName);
+            }
+            foreach (var cc in file.ChoosedComponents)
+            {
+                selectComponentGrid.Items.Add(cc);
+            }
+        }
         private void WordButton_Click(object sender, RoutedEventArgs e)
         {
             //using (var dialog = new SaveFileDialog { Filter = "docx|*.docx" })
